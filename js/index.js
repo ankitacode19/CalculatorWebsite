3586d7854bfe885af18cd28826d5901d68dc3926
Ankita Yadav <ankitacoder2004@gmail.com>
2020-10-10 13:06:53 +0530

    calculator made

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 function insert(num) {
   document.form.textview.value = document.form.textview.value + num;
 }
+
+function equal() {
+  var exp = document.form.textview.value;
+  if (exp) {
+    document.form.textview.value = eval(exp);
+  }
+}
